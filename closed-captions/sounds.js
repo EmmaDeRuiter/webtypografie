00b65a7a8d9e9842681cd12db9fb88f350145b35
EmmaDeRuiter <63364613+EmmaDeRuiter@users.noreply.github.com>
2020-05-19 12:27:59 +0200

    jeej

---
+++ closed-captions/sounds.js
@@ -0,0 +1,22 @@
+// Hier kan je tijden toevoegen in secondes
+// Op die mommenten komt er een class op de body.
+sounds = [
+	0,
+	9,
+	12,
+	15,
+	16.5,
+    19,
+    20,
+    21,
+    23,
+	24,
+	25,
+    26.5,
+    28,
+	31,
+	32,
+    33,
+	34,
+	86
+];
